9daf6cfb08ccc7e2b39b5c5ad5cc15f9e13b3286
steve yeadon <ax4413@gmail.com>
2018-06-08 15:14:30 +0100

    made set-FilePermissions location aware

--- Posh/Scripts/Helpfull-Console-Utils.ps1
+++ Posh/Scripts/Helpfull-Console-Utils.ps1
@@ -14,15 +14,55 @@ function touch($path){
 
 
 # Fix issues with the file system
-function Set-FilePermisions($path, $user ="Users"){
-	$args = @( $path 
-		 , "/grant"
-		 , ":r"
-		 , "$user`:(OI)(CI)F"
-		 , "/T" )
-	
+function Set-FilePermissions
+{
+  [CmdletBinding()]
+  Param
+  (
+    # the path to fix the file permissions for
+    [Parameter(Mandatory=$false,
+                ValueFromPipelineByPropertyName=$true,
+                Position=0)]
+    [string] $path, 
+
+    # the user/group to assign permissions to
+    [Parameter(Mandatory=$false,
+                ValueFromPipelineByPropertyName=$true,
+                Position=1)]
+    [string] $user ="Users"
+  )
+
+  Begin
+  {
+  }
+  Process
+  {
+    if(-not $path){
+      Write-Verbose "No location supplied using the current location"
+      $path = Get-Location | Select-Object -ExpandProperty Path
+    } 
+    
+    if(-not (Test-Path $path -PathType Container)) {
+      Write-Error "Error - Invalid path supplied $path"
+    }
+    
+    $args = @(  $path 
+              , "/grant"
+              , ":r"
+              , "$user`:(OI)(CI)F"
+              , "/T"  )
+
     write-host "Executing - icacls $args" -ForegroundColor Green
     #EchoArgs.exe icacls $args
     
     icacls $args
+
+    if($LASTEXITCODE -ne 0){
+      Write-Error "Error - icacls failed with error code '$LASTEXITCODE'"
+    }
+  }
+  End
+  {
+  }
 }
+
